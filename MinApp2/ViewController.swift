5fae938e36638244974288c1572bfadb198b1b94
COE-18 <coe-18@expert-imac.local>
2019-09-20 15:19:23 +0530

    done

--- MinApp2/ViewController.swift
+++ MinApp2/ViewController.swift
@@ -8,8 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+{
 
+    var menu:[[String:String]] = [
+        
+        ["Title":"Sign Up","Controller":"redScene"],
+        ["Title":"Sign In","Controller":"greenScene"]
+
+    ]
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return menu.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellScene", for: indexPath)
+        
+         cell.textLabel?.text = menu[indexPath.section]["Title"]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+        let controller = self.storyboard?.instantiateViewController(withIdentifier: menu[indexPath.section]["Controller"]!)
+        self.navigationController?.pushViewController(controller!, animated: true)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
